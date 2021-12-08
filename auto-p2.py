018fd5460350a7fb303a946c1d0792cafd910a2f
pilarpetru <95748914+pilarpetru@users.noreply.github.com>
2021-12-08 14:06:23 +0100

    Update auto-p2.py

--- auto-p2.py
+++ auto-p2.py
@@ -6,6 +6,16 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('auto-p2')
-Hola amigos
 
+def prepare(nsw = 3):
+    try:
+        nsw=int(nsw)
+        if(1>nsw or 5<nsw):
+            print("El numero de servidores web tiene que estar entre 1 y 5")
+            sys.exit()
+    except:
+        sys.exit()
+    for i in range(0, nsw):
+        call(["qemu-image", "create", "-f", "qcow2", "-b", "cdps-vm-base-p2.qcow2" "s{}.qcow2".format(i)] )
+        call(["cp", "plantilla-vm-p2.xml", "s{}.xml".format(i)])
 
